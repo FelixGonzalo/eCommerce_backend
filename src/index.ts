a9666088da3c8f7fbe6413eaee32cc5572702181
Felix Castro <felixgonzalocastro@gmail.com>
2022-05-23 18:20:39 -0500

    ADD api documentation

--- src/index.ts
+++ src/index.ts
@@ -5,13 +5,16 @@ import productsRouter from './api/products/controller'
 import shoppingCartsRouter from './api/shoppingCarts/controller'
 import { handleUnknownRoutes } from './network/errors'
 import { errorHandler } from './network/errors'
+import swaggerUI from 'swagger-ui-express'
+const swaggerDoc = require('./api/swagger.json')
 
 const app = express()
 app.use(express.json())
 app.use('/api/products', productsRouter)
 app.use('/api/shoppingCarts', shoppingCartsRouter)
-app.use('*', handleUnknownRoutes)
+app.use('/', swaggerUI.serve, swaggerUI.setup(swaggerDoc))
 app.use(errorHandler)
+app.use('*', handleUnknownRoutes)
 
 app.listen(config.API_PORT, () => {
   console.log(`Server open on PORT: ${config.API_PORT}`)
