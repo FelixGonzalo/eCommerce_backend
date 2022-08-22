fe590f4e003441408e109542d6e587d300263d41
Felix Castro <felixgonzalocastro@gmail.com>
2022-08-22 00:28:15 -0500

    ADD jwt for user token

--- src/store/mongoDb/models/ShoppingCart.ts
+++
@@ -1,8 +0,0 @@
-import { Schema, model } from 'mongoose'
-import { productSchema } from './Product'
-
-const ShoppingCartSchema = new Schema({
-  products: [productSchema],
-})
-
-export const ShoppingCart = model('ShoppingCart', ShoppingCartSchema)
