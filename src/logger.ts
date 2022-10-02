f17714cb941c1614f596cda74b39af90d9cddf97
Felix Castro <felixgonzalocastro@gmail.com>
2022-10-01 21:54:45 -0500

    FIX generate logs outside the build folder

--- src/logger.ts
+++ src/logger.ts
@@ -1,12 +1,12 @@
 import { createLogger, format, transports } from 'winston'
-
+console.log('__dirname', __dirname)
 export default createLogger({
   transports: [
     new transports.File({
       level: 'info',
       maxsize: 1000 * 1000 * 5,
       maxFiles: 5,
-      filename: `${__dirname}/../logs/log-${
+      filename: `./logs/log-${
         process.argv[2].split('=')[1] || 'development'
       }.log`,
     }),
